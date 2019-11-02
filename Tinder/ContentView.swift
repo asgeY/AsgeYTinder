173e94f792b1f12be60a452fe304b84ea1cbd39d
Asgedom <asmow@yahoo.com>
2019-11-02 03:14:12 -0500

    Tinder APP Using swiftUI created also firebase

--- Tinder/ContentView.swift
+++ Tinder/ContentView.swift
@@ -7,15 +7,53 @@
 //
 
 import SwiftUI
+import Firebase
+import SDWebImageSwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TopView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TopView: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                
+                
+            }) {
+                Image("person")
+                    .resizable()
+                    .frame(width: 35, height: 35)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                Image(systemName: "flame.fill")
+                    .resizable()
+                    .frame(width: 30, height: 35)
+                    .foregroundColor(.red)
+            }
+            Spacer()
+            Button(action: {
+                
+            }) {
+                Image("chat")
+                    .resizable()
+                    .frame(width: 35, height: 35)
+                    .foregroundColor(.gray)
+            }
+        .padding()
+        }
     }
 }
